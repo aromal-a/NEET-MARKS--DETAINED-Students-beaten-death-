40978cec6aa244f13d8345d2fe4063cc636369a0
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-07-23 21:27:21 +0530

    Update and rename Fakecop to Fakecop.c

---
+++ Fakecop.c
@@ -0,0 +1,25 @@
+No name-plate
+No -Id : Leak-proof()
+
+Understand agenda by verbal - > 'Forsure he don't understand any' => Infact-They don't know why they are there.
+Their blood knows only revenge and students are the victim of their abuse : 
+
+<Please put those cops under Lime-light and make them say their motives while doing so>
+
+
+Under-Agent :  Cop-call : fake-Bistro : <Army Bistro>[Distro from Meesho , upliftment(old-criminal , goons , Just for Daypay)]
+[Police can't be trusted forr neet ,  It is provoked that they don't posses nor legal knowledge nor fair practices to managed crowd]
+
+
+Distro -> [on student's body ? Fake and Not listed : <Old goons dresses up as Cops>]
+
+
+supply  by central sole occupation : <Children abusing> [Started from child hood]
+
+[If by central start means occupation , Them like perpretators , donot know actual problem side , TG:- Information , encrypted-privacy = 'Non-existent']
+
+
+  [Name by sane , id -: AC{AAADHAR:card} : occupation(Constant , [You are not a Police but pretending])]
+
+
+
